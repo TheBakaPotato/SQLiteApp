1b885603362dc948d2cd31b3f2c160b2fb84ec67
TheBakaPotato <TheBakaPotato@thebakapotato.com>
2022-01-08 23:54:40 -0300

    Test

--- app/src/main/java/com/example/sqliteapp/MainActivity.java
+++ app/src/main/java/com/example/sqliteapp/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.sqliteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn_viewAll, btn_add;
+    EditText et_name, et_age;
+    Switch sw_active;
+    ListView lv_customerList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_add = findViewById(R.id.btn_add);
+        btn_viewAll = findViewById(R.id.btn_viewAll);
+        et_age = findViewById(R.id.et_age);
+        et_name = findViewById(R.id.et_name);
+        sw_active = findViewById(R.id.sw_active);
+        lv_customerList = findViewById(R.id.lv_customerList);
+
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomerModel customerModel = new CustomerModel(-1, et_name.getText().toString(), Integer.parseInt(et_age.getText().toString()), sw_active.isChecked());
+                Toast.makeText(MainActivity.this, customerModel.toString(), Toast.LENGTH_SHORT).show();
+
+            }
+        });
+        btn_viewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "You clicked the View All Button", Toast.LENGTH_SHORT).show();                
+            }
+        });
     }
 }
